1de67e330ec4175f13d0594b0033dd3d4371d50b
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2023-08-27 16:12:05 +0800

    Create word_counter.py

---
+++ word_counter.py
@@ -0,0 +1,36 @@
+# Word Counter Challenge
+
+# Welcome message
+print("Welcome to the Word Counter Challenge!")
+
+# Function to count words in a file
+def count_words(file_path):
+    """
+    Counts the total number of words in a file and returns the count.
+    """
+    # TODO: Implement the word counting logic here
+    pass
+
+# Function to count word frequencies in a file
+def count_word_frequencies(file_path):
+    """
+    Counts the frequency of each word in a file and returns a dictionary with word frequencies.
+    """
+    # TODO: Implement the word frequency counting logic here
+    pass
+
+# Main function
+def main():
+    """
+    Main function to run the Word Counter Challenge.
+    """
+    # Get file path from user
+    file_path = input("Enter the path to the file: ")
+
+    # TODO: Call the count_words and count_word_frequencies functions with the file_path
+
+    # TODO: Display the total number of words and word frequency count
+
+# Run the main function
+if __name__ == "__main__":
+    main()
