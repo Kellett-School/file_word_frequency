de07ee64c850656ef5263179ba86c2b2bc77a7f4
Mr Davies <82430508+MrDaviesKellett@users.noreply.github.com>
2023-08-27 17:37:37 +0800

    Update word_counter.py
    add two additional functions 'count_unique_words' and 'most_common_words' and add more explicit comments listing arguments and return values.

--- word_counter.py
+++ word_counter.py
@@ -3,18 +3,55 @@
 # Welcome message
 print("Welcome to the Word Counter Challenge!")
 
-# Function to count words in a file
-def count_words(file_path):
+def count_words(text):
     """
-    Counts the total number of words in a file and returns the count.
+    Counts the number of words in the given text.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        int: The number of words in the text.
     """
-    # TODO: Implement the word counting logic here
+    # Implementation goes here
+    pass
+
+def count_unique_words(text):
+    """
+    Counts the number of unique words in the given text.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        int: The number of unique words in the text.
+    """
+    # Implementation goes here
+    pass
+
+def most_common_words(text):
+    """
+    Finds the most common word(s) in the given text.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        list: A list of the most common word(s) in the text.
+    """
+    # Implementation goes here
     pass
 
 # Function to count word frequencies in a file
-def count_word_frequencies(file_path):
+def count_word_frequencies(text):
     """
     Counts the frequency of each word in a file and returns a dictionary with word frequencies.
+    
+    Args:
+        text (str): The input text.
+
+    Returns:
+        list: A dictionary of all words and how many times they occur.
     """
     # TODO: Implement the word frequency counting logic here
     pass
